9bddbceee88fc2740305f86ab669ba87d3c06193
Guillermo Davidov <gdavidov@velocitypartners.net>
2017-09-04 17:13:20 -0300

    Adding More UI Tweaks

--- DondeCargoNafta/DondeCargoNafta/Models/RateViewModel.cs
+++ DondeCargoNafta/DondeCargoNafta/Models/RateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using DondeCargoNafta.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -10,12 +11,16 @@ namespace DondeCargoNafta.Models
     {
         public Station Station { get; set; }
 
+        [DisplayName("Nafta Super")]
         public double RegularGas { get; set; }
 
+        [DisplayName("Nafta Premium")]
         public double PremiumGas { get; set; }
 
+        [DisplayName("Diesel Común")]
         public double RegularDiesel { get; set; }
 
+        [DisplayName("Diesel Premium")]
         public double PremiumDiesel { get; set; }
     }
 }

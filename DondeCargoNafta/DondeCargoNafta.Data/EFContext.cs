8b3a5e848a64b762930c4aaebbc8fbe648c809cd
Andres Virga <andvirga@gmail.com>
2017-09-04 12:06:06 -0300

    Add some EF data

--- DondeCargoNafta/DondeCargoNafta.Data/EFContext.cs
+++ DondeCargoNafta/DondeCargoNafta.Data/EFContext.cs
@@ -4,8 +4,35 @@ using DondeCargoNafta.Entities;
 
 namespace DondeCargoNafta.Data
 {
-    public class EFContext: DbContext
+    public class EFContext : DbContext
     {
         public DbSet<Station> Stations { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Brand> Brands
+        {
+            get; set;
+        }
+
+        public DbSet<Fuel> Fuels
+        {
+            get; set;
+        }
+
+        public DbSet<FuelPrice> FuelPrices
+        {
+            get; set;
+        }
+
+        public DbSet<Location> Locations
+        {
+            get; set;
+        }
+
+        public DbSet<Price> Prices
+        {
+            get; set;
+        }
     }
 }

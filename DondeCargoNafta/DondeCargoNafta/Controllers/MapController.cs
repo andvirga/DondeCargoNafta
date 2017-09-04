6c1c8c88fb57b018005b660c2d7de219b2c1c276
Andres Virga <andvirga@gmail.com>
2017-09-04 09:45:40 -0300

    Added Google Maps API

---
+++ DondeCargoNafta/DondeCargoNafta/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DondeCargoNafta.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Map()
+        {
+            return View("Map");
+        }
+    }
+}

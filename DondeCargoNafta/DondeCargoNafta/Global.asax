8e56da72b3daa47a2d98e497d6dcb3c6d21e9e24
Iván Falletti <ivan_falletti@hotmail.com>
2017-09-04 09:26:00 -0300

    initial commit

---
+++ DondeCargoNafta/DondeCargoNafta/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DondeCargoNafta.MvcApplication" Language="C#" %>
---
+++ DondeCargoNafta/DondeCargoNafta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DondeCargoNafta.Startup))]
+namespace DondeCargoNafta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
